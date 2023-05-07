ce5a70bc51e4debf364f8cc6ae3fe873a26697d4
elmehdirida <elmehdi.fst@uhp.ac.ma>
2023-05-07 15:59:28 +0100

    modified find list note

---
+++ src/app/services/filiereService/filiere.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {Filiere} from "../../Model/filiere.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FiliereService {
+
+  constructor(private http:HttpClient) { }
+
+  public getFilieres():Observable<Array<Filiere>>{
+    return this.http.get<Array<Filiere>>(environment.apiFiliere);
+  }
+}
---
+++ src/app/services/moduleService/module.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Module} from "../../Model/module.model";
+import {environment} from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModuleService {
+
+  constructor(private http:HttpClient) { }
+  public  GetModules():Observable<Array<Module>>{
+    return this.http.get<Array<Module>>(environment.apiModule);
+  }
+}
+
+
