17355428247962d0159b8972fbf08cebe9edad36
KHAYAATI <mohamed.khayati101@gmail.com>
2023-06-17 19:49:00 +0200

    gestion des etudiats v2

---
+++ src/app/GestionStudent/Models/student1.ts
@@ -0,0 +1,16 @@
+export interface Student1 {
+    nom:String;
+    prenom:String;
+    email:String;
+    adresse:String;
+    phone:String;
+    cin:String;
+    cne:String;
+    apogee:number;
+    dateNaissance:Date;
+    lieuNaissance:String;
+    genre:String;
+    filiere:String;
+    carriers: String[];
+
+}
---
+++ src/app/GestionStudent/Services/student-services.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Student1 } from '../Models/student1';
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentServicesService {
+
+
+
+
+  constructor(private httpClient: HttpClient) { }
+  getStudentsList():Observable<Student1[]>{
+    return this.httpClient.get<Student1[]>(`${environment.apiStudent}/getStudents`)
+}
+
+public updateStudent(id:any ,Student1: any){
+  return this.httpClient.put(`${environment.apiStudent}/updateStudent/${id}`,Student1);
+}
+
+public deleteStudent(id: number) {
+  return this.httpClient.delete(`${environment.apiStudent}/${id}`);
+}
+
+}
